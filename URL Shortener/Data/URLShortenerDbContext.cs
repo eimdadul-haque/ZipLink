2c77469277d016e073480dd5160833dc0d032e85
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-02-01 01:08:05 +0600

    db contetx and urls added

---
+++ URL Shortener/Data/URLShortenerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URL_Shortener.Models.Entities;
+
+namespace URL_Shortener.Data
+{
+    public class URLShortenerDbContext : DbContext
+    {
+        public URLShortenerDbContext(DbContextOptions<URLShortenerDbContext> options) 
+            : base(options) { }  
+        
+        public DbSet<Url> Urls { get; set; }
+    }
+}
