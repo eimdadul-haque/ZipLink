574baa1d0ea5402001f3fd173bd315194b6157b3
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-02-01 01:07:35 +0600

    url shortener service added

---
+++ URL Shortener/Services/URLShortenerService.cs
@@ -0,0 +1,15 @@
+﻿namespace URL_Shortener.Services
+{
+    public class URLShortenerService
+    {
+        public int maxUrlLenght = 7;
+        public const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private readonly Random random = new Random();
+
+        public async Task<string> GenerateUniqueCode()
+        {
+
+            return string.Empty;
+        }
+    }
+}
