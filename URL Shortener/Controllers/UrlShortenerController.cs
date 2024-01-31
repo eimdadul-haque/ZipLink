37b5459d7143c07ff23e7db4a1ac67f8ada02399
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-02-01 01:06:56 +0600

    controller added

---
+++ URL Shortener/Controllers/UrlShortenerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using URL_Shortener.Models.Dtos;
+
+namespace URL_Shortener.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class UrlShortenerController : ControllerBase
+    {
+        public UrlShortenerController() { }
+
+        public async Task<IActionResult> Short(UrlShortenerDto request)
+        {
+            if (Uri.TryCreate(request.Url, UriKind.Absolute, out _))
+            {
+                return BadRequest("Invaild url!");
+            }
+
+            return Ok();
+        }
+    }
+}
