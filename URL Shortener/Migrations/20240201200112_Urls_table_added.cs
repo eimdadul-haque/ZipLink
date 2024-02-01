52f12cf2a2dbebfd724bf9ec70a9d6042efe795d
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-02-02 02:53:25 +0600

    migration added for url model

---
+++ URL Shortener/Migrations/20240201200112_Urls_table_added.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URL_Shortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class Urls_table_added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RealUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortUrl = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CreatedOnUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_Code",
+                table: "Urls",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urls");
+        }
+    }
+}
