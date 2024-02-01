cf10a6f550d3dff2a0ce89647ef05d9426147c94
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-02-02 01:33:32 +0600

    method added for generate code

--- URL Shortener/Services/URLShortenerService.cs
+++ URL Shortener/Services/URLShortenerService.cs
@@ -1,15 +1,44 @@
-﻿namespace URL_Shortener.Services
+﻿using Microsoft.EntityFrameworkCore;
+using URL_Shortener.Data;
+
+namespace URL_Shortener.Services
 {
     public class URLShortenerService
     {
-        public int maxUrlLenght = 7;
-        public const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        public const int maxUrlLenght = 7;
+        public const string alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         private readonly Random random = new Random();
+
+        private readonly URLShortenerDbContext _dbContext;
+        public URLShortenerService(URLShortenerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
         public async Task<string> GenerateUniqueCode()
         {
+            while (true)
+            {
+                var codeChars = new char[maxUrlLenght];
 
-            return string.Empty;
+                for (int i = 0; i < codeChars.Length; i++)
+                {
+                    int randomIndex = random
+                        .Next(0, codeChars.Length - 1);
+
+                    codeChars[i] = alphabets[randomIndex];
+                }
+
+                string code = new string(codeChars);
+
+                var isExist = await _dbContext.Urls
+                    .AnyAsync(x => x.Code == code);
+
+                if (!isExist)
+                {
+                    return code;
+                }
+            }
         }
     }
 }
