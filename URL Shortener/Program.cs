71ec62bd9d4c2fc3799c73b9d619b8e39177a837
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-02-02 01:43:35 +0600

    register service in dj life cycle

--- URL Shortener/Program.cs
+++ URL Shortener/Program.cs
@@ -1,24 +1,30 @@
 using Microsoft.EntityFrameworkCore;
 using URL_Shortener.Data;
+using URL_Shortener.IService;
+using URL_Shortener.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication
+    .CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<URLShortenerDbContext>((option) =>
-{
-    option.UseSqlServer(builder.Configuration.GetConnectionString(""));
-});
+builder.Services
+    .AddControllers();
 
-var app = builder.Build();
+builder.Services
+    .AddDbContext<URLShortenerDbContext> (option => option
+    .UseSqlServer(builder.Configuration
+    .GetConnectionString("Default")));
+
+builder.Services
+    .AddTransient<IURLShortenerService, URLShortenerService>();
+
+var app = builder
+    .Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
