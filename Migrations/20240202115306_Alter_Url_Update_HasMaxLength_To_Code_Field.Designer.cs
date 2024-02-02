5843244cd6d8e04648a3827adcb915370e7e3829
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-02-02 17:58:18 +0600

    migration update for code field

--- Migrations/20240202115306_Alter_Url_Update_HasMaxLength_To_Code_Field.Designer.cs
+++ Migrations/20240202115306_Alter_Url_Update_HasMaxLength_To_Code_Field.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using URL_Shortener.Data;
 
@@ -11,9 +12,11 @@ using URL_Shortener.Data;
 namespace URL_Shortener.Migrations
 {
     [DbContext(typeof(URLShortenerDbContext))]
-    partial class URLShortenerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240202115306_Alter_Url_Update_HasMaxLength_To_Code_Field")]
+    partial class Alter_Url_Update_HasMaxLength_To_Code_Field
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240202115306_Alter_Url_Update_HasMaxLength_To_Code_Field.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URL_Shortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alter_Url_Update_HasMaxLength_To_Code_Field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortUrl",
+                table: "Urls",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(7)",
+                oldMaxLength: 7);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Urls",
+                type: "nvarchar(7)",
+                maxLength: 7,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortUrl",
+                table: "Urls",
+                type: "nvarchar(7)",
+                maxLength: 7,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Urls",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(7)",
+                oldMaxLength: 7);
+        }
+    }
+}
