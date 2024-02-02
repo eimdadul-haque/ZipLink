41b427d85d2b309b34f43bfcd37577434cff75b8
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-02-02 17:56:57 +0600

    seri log service added

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 using URL_Shortener.Data;
 using URL_Shortener.IService;
+using URL_Shortener.Middlewares;
 using URL_Shortener.Services;
 
 var builder = WebApplication
     .CreateBuilder(args);
 
 // Add services to the container.
+
+Log.Logger = new LoggerConfiguration()
+#if DEBUG
+    .MinimumLevel.Debug()
+#else
+    .MinimumLevel.Information()
+#endif
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.File(
+        "logs/.txt",
+        rollingInterval: RollingInterval.Day,
+        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning)
+    .CreateLogger();
+
+builder.Host
+    .UseSerilog();
 
 builder.Services
     .AddControllers();
@@ -18,7 +39,7 @@ builder.Services
     .AddHttpContextAccessor();
 
 builder.Services
-    .AddDbContext<URLShortenerDbContext> (option => option
+    .AddDbContext<URLShortenerDbContext>(option => option
     .UseSqlServer(builder.Configuration
     .GetConnectionString("Default")));
 
@@ -30,13 +51,13 @@ var app = builder
 
 // Configure the HTTP request pipeline.
 
-app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-});
+app.UseSwaggerUI(options
+    => options.SwaggerEndpoint("v1/swagger.json", "v1"));
 
+app.UseMiddleware<GlobalErrorHandler>();
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.UseSwagger();
 app.Run();
