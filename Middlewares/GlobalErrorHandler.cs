c7bb993d9adb6bd042309eb024ef420bf527fa56
Eimdadul Haque <eimdadulhaque@gmail.com>
2024-02-02 17:58:51 +0600

    custom middleware added for global error handle

---
+++ Middlewares/GlobalErrorHandler.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace URL_Shortener.Middlewares
+{
+    public class GlobalErrorHandler
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalErrorHandler> _logger;
+        public GlobalErrorHandler(
+            ILogger<GlobalErrorHandler> logger,
+            RequestDelegate next)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex, ex.Message);
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+        }
+    }
+}
